23b4e13f76dc5955cbdc0c265184b8c6b15e0fcb
unnatii <unnatitibrewal@gmail.com>
2017-07-02 20:56:09 +0530

    clock addd

--- home.js
+++ home.js
@@ -64,8 +64,14 @@ var i;
  
 
 }
+ 
+var myVar = setInterval(function(){ myTimer() }, 1000);
 
-
+function myTimer() {
+    var d = new Date();
+    var t = d.toLocaleTimeString();
+    document.getElementById("clock").innerHTML = t;
+}
 
   
 
